590ef5b52f300705579f06e2d24f05c474069275
unknown <zjomzjom@gmail.com>
2020-05-15 10:56:07 +0200

    initial commit: first project

---
+++ branch/UsersMicroservice/Services/UsersService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace UsersMicroservice
+{
+    public class UsersService : Users.UsersBase
+    {
+        private readonly ILogger<UsersService> _logger;
+        public UsersService(ILogger<UsersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<TokenReply> Token(SignInRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new TokenReply{
+                Token = new Guid().ToString()
+            });
+        }
+
+        public override Task<Empty> Logout(LogoutRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> AddMessage(AddMessageRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new Empty());
+        }
+    }
+}
