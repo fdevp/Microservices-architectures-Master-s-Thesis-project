c62cb5a3b49695e65d3f399fc9be155f3915af0b
unknown <zjomzjom@gmail.com>
2020-05-22 17:49:55 +0200

    set port for each service, added grpc clients initialization in  api gateway

--- branch/APIGateway/Startup.cs
+++ branch/APIGateway/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Grpc.Net.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using static AccountsMicroservice.Accounts;
+using static BatchesBranchMicroservice.BatchesBranch;
+using static CardsMicroservice.Cards;
+using static LoansMicroservice.Loans;
+using static PanelsBranchMicroservice.PanelsBranch;
+using static PaymentsMicroservice.Payments;
+using static ReportsBranchMicroservice.ReportsBranch;
+using static TransactionsMicroservice.Transactions;
+using static UsersMicroservice.Users;
 
 namespace APIGateway
 {
@@ -46,6 +57,33 @@ namespace APIGateway
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void ConfigureGrpcConnections(IServiceCollection services)
+        {
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var httpClient = new HttpClient(httpClientHandler);
+
+
+            services.AddSingleton(new TransactionsClient(CreateChannel(httpClient, "localhost", "5001")));
+            services.AddSingleton(new AccountsClient(CreateChannel(httpClient, "localhost", "5011")));
+            services.AddSingleton(new UsersClient(CreateChannel(httpClient, "localhost", "5012")));
+            services.AddSingleton(new PaymentsClient(CreateChannel(httpClient, "localhost", "5013")));
+            services.AddSingleton(new CardsClient(CreateChannel(httpClient, "localhost", "5014")));
+            services.AddSingleton(new LoansClient(CreateChannel(httpClient, "localhost", "5015")));
+
+            services.AddSingleton(new ReportsBranchClient(CreateChannel(httpClient, "localhost", "5011")));
+            services.AddSingleton(new PanelsBranchClient(CreateChannel(httpClient, "localhost", "5012")));
+            services.AddSingleton(new BatchesBranchClient(CreateChannel(httpClient, "localhost", "5013")));
+        }
+
+        private GrpcChannel CreateChannel(HttpClient httpClient, string host, string port)
+        {
+            var address = $"https://{host}:{port}";
+            var channel = GrpcChannel.ForAddress(address, new GrpcChannelOptions { HttpClient = httpClient });
+            return channel;
         }
     }
 }
