507fa9a796fd6f024fbf1e11ff09173c017b2021
unknown <zjomzjom@gmail.com>
2020-08-07 20:38:51 +0200

    added branch transactions loadbalancer

---
+++ branch/TransactionsLoadBalancer/Services/TransactionsService.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using TransactionsMicroservice;
+using static TransactionsMicroservice.Transactions;
+
+namespace TransactionsLoadBalancer
+{
+    public class TransactionsService : Transactions.TransactionsBase
+    {
+        private readonly ILogger<TransactionsService> _logger;
+        private readonly TransactionsClient[] services;
+
+        public TransactionsService(ILogger<TransactionsService> logger, IEnumerable<TransactionsClient> services)
+        {
+            _logger = logger;
+            this.services = services.ToArray();
+        }
+
+        public override async Task<GetTransactionsResult> Get(GetTransactionsRequest request, ServerCallContext context)
+        {
+            var groupedIds = request.Ids.GroupBy(id => GetServiceIndex(id)).ToArray();
+            var tasks = groupedIds.Select(g => services[g.Key].GetAsync(new GetTransactionsRequest { FlowId = request.FlowId, Ids = { g } }).ResponseAsync);
+
+            var results = await Task.WhenAll(tasks);
+            var transactions = results.SelectMany(r => r.Transactions);
+            return new GetTransactionsResult { Transactions = { transactions } };
+        }
+
+        public override async Task<CreateTransactionResult> Create(CreateTransactionRequest request, ServerCallContext context)
+        {
+            var service = services[GetServiceIndex(request.FlowId)];
+            var transaction = await service.CreateAsync(request);
+            return transaction;
+        }
+
+        public override async Task<BatchCreateTransactionResult> BatchCreate(BatchCreateTransactionRequest request, ServerCallContext context)
+        {
+            var groupedRequests = request.Requests.GroupBy(r => GetServiceIndex(Guid.NewGuid()));
+            var tasks = groupedRequests.Select(g => services[g.Key].BatchCreateAsync(new BatchCreateTransactionRequest { FlowId = request.FlowId, Requests = { g } }).ResponseAsync);
+            var results = await Task.WhenAll(tasks);
+            return new BatchCreateTransactionResult { Transactions = { results.SelectMany(r => r.Transactions) } };
+        }
+
+        public override async Task<GetTransactionsResult> Filter(FilterTransactionsRequest request, ServerCallContext context)
+        {
+            var tasks = services.Select(s => s.FilterAsync(request).ResponseAsync);
+            var results = await Task.WhenAll(tasks);
+            return new GetTransactionsResult { Transactions = { results.SelectMany(r => r.Transactions) } };
+        }
+
+        public override async Task<Empty> Setup(SetupRequest request, ServerCallContext context)
+        {
+            var groupedIds = request.Transactions.GroupBy(t => GetServiceIndex(t.Id)).ToArray();
+            var tasks = groupedIds.Select(g => services[g.Key].SetupAsync(new SetupRequest { Transactions = { g } }).ResponseAsync);
+            await Task.WhenAll(tasks);
+            return new Empty();
+        }
+
+        public override async Task<Empty> SetupAppend(SetupRequest request, ServerCallContext context)
+        {
+            var groupedIds = request.Transactions.GroupBy(t => GetServiceIndex(t.Id)).ToArray();
+            var tasks = groupedIds.Select(g => services[g.Key].SetupAppendAsync(new SetupRequest { Transactions = { g } }).ResponseAsync);
+            await Task.WhenAll(tasks);
+            return new Empty();
+        }
+
+        private int GetServiceIndex(string guid) => GetServiceIndex(new Guid(guid));
+        private int GetServiceIndex(Guid guid) => Math.Abs(guid.GetHashCode()) % services.Length;
+    }
+}
