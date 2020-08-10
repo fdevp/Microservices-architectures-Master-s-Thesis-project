b7261147f4fb133a4a4b9a049d9834b07b6e3b24
unknown <zjomzjom@gmail.com>
2020-08-10 14:17:02 +0200

    requester automat mode work in progress

---
+++ Requester/Requester/Data/AutomatSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Requester.Data
+{
+    public class AutomatSettings
+    {
+        public int TotalCount { get; set; }
+        public int Offset { get; set; }
+        public TimeSpan TimeDelta { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public DateTime DateLimit { get; set; }
+    }
+}
--- Requester/Requester/RunningModes/AutomatMode.cs
+++ Requester/Requester/RunningModes/AutomatMode.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Jil;
+using Requester.Data;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,9 +11,78 @@ namespace Requester.RunningModes
 {
     public class AutomatMode
     {
+        private readonly HttpClient httpClient;
+        private readonly Settings settings;
+        private readonly AutomatSettings automatSettings;
+
+        public AutomatMode(HttpClient httpClient, Settings settings, AutomatSettings automatSettings)
+        {
+            this.httpClient = httpClient;
+            this.settings = settings;
+            this.automatSettings = automatSettings;
+        }
+
         public void Perform()
         {
-            
+            Parallel.For(0, settings.Threads, i => Perform(i));
+        }
+
+        private void Perform(int index)
+        {
+            var currentTime = automatSettings.CurrentDate;
+            var data = GetData(index);
+            var toPay = data.Payments.Where(p => ShouldBePaid(p));
+
+            var balancesDict = data.Balances.ToDictionary(k => k.Id, v => v);
+            var loansDict = data.Loans.ToDictionary(k => k.PaymentId, v => v);
+
+            var withSufficientBalance = toPay.Where(p => p.Amount <= balancesDict[p.AccountId].Amount).ToArray();
+            var withInsufficientBalance = toPay.Except(withSufficientBalance);
+            var paidIds = withSufficientBalance.Select(p => p.Id).ToHashSet();
+
+            var transfers = withSufficientBalance.Select(p => CreateTransfer(p, loansDict.ContainsKey(p.Id) ? loansDict[p.Id] : null)).ToArray();
+            var messages = withInsufficientBalance.Select(p => CreateMessage(p, balancesDict[p.AccountId])).ToArray();
+            var repaidInstalments = data.Loans.Where(l => paidIds.Contains(l.PaymentId)).Select(l => l.Id).ToArray();
+        }
+
+        private bool ShouldBePaid(PaymentDTO payment, DateTime currentTime)
+        {
+
+        }
+
+        private BatchData GetData(int index)
+        {
+            var url = $"Batch?part={index}&total={automatSettings.TotalCount}";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var scenarioId = Guid.NewGuid().ToString();
+            request.Headers.Add("flowId", scenarioId);
+            var result = httpClient.SendAsync(request).Result;
+            var response = result.Content.ReadAsStringAsync().Result;
+            return JSON.Deserialize<BatchData>(response);
+        }
+
+        private AccountTransfer CreateTransfer(PaymentDTO payment, LoanDTO loan)
+        {
+            var title = loan != null ? CreateLoanTitle(loan) : $"Transaction of payment {payment.Id}";
+            return new AccountTransfer
+            {
+                AccountId = payment.AccountId,
+                Amount = payment.Amount,
+                Recipient = payment.Recipient,
+                Title = title
+            };
+        }
+
+        private string CreateLoanTitle(LoanDTO loan)
+        {
+            var currentlyPaid = (loan.PaidAmount / loan.TotalAmount) * loan.Instalments;
+            return $"{currentlyPaid + 1} of {loan.Instalments} instalments. Payment {loan.PaymentId}";
+        }
+
+        private MessageDTO CreateMessage(PaymentDTO payment, BalanceDTO balance)
+        {
+            var content = $"There is insufficient balance on account {balance.Id} to repay payment {payment.Id}. Required balance: {payment.Amount}, current balance: {balance.Amount}";
+            return new MessageDTO { UserId = "", Content = content };
         }
     }
 }
